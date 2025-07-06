5f3b9f72e5962e83be5b036bb0a296508eba9380
Melody4063 <kuyet10402@bazeuniversity.edu.ng>
2025-07-06 15:24:11 +0000

    Update Program.cs

--- Regex/Timeouts/Program.cs
+++ Regex/Timeouts/Program.cs
@@ -1,15 +1,15 @@
-﻿// LinkedIn Learning Course .NET Programming with C# by Joe Marini
+// LinkedIn Learning Course .NET Programming with C# by Joe Marini
 // Using Timeout settings for RegEx in .NET
 using System.Text.RegularExpressions;
 using System.Diagnostics;
-
+const int MAX_REGEX_TIME = 1000;
 const string thestr = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
 // Use a Stopwatch to show elapsed time
 Stopwatch sw;
 
 // TODO: Use a Timeout value when executing RegEx to guard against bad input
-
+TimeSpan Timeout = TimeSpan.FromMilliseconds(MAX_REGEX_TIME);
 
 // Run the expression and output the result
 try {
@@ -22,6 +22,6 @@ try {
         Console.WriteLine($"'{match.Value}' found at position {match.Index}");
     }
 }
-catch (Exception e) {
-    Console.WriteLine(e);
+catch (RegexMatchTimeoutException e) {
+Console.WriteLine($"The Regex took too long to execute! {e.MatchTimeout}");
 }
