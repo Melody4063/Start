2da46bdbbd41961104ec4603971710d138b6f39e
Melody4063 <kuyet10402@bazeuniversity.edu.ng>
2025-07-06 15:25:42 +0000

    Update Program.cs

--- Strings/Interpolation/Program.cs
+++ Strings/Interpolation/Program.cs
@@ -1,4 +1,4 @@
-﻿// Application Programming .NET Programming with C# by Abdullahi Tijjani
+// Application Programming .NET Programming with C# by Abdullahi Tijjani
 
 // String Interpolation is a feature that enables the easy insertion of data
 // and expression values into a string variable
@@ -10,11 +10,13 @@ string c = "CSharp";
 // String output the old way - using placeholders
 Console.WriteLine("The values are {0}, {1} and {2}", a, b, c);
 
-// TODO: Using string interpolation, the code is much easier to read
+// Using string interpolation, the code is much easier to read
+Console.WriteLine($"The values are {a}, {b} and {c}");
 
+// Interpolated strings can contain expressions as well
+Console.WriteLine($"(a + b)/b is {(a + b)/b}");
+Console.WriteLine($"{c} in upper-case is {c.ToUpper()}");
 
-// TODO: Interpolated strings can contain expressions as well
-
-
-// TODO: Complex objects can be embedded in strings this way as well
-
+// Complex objects can be embedded in strings this way as well
+DateTime now = DateTime.Now;
+Console.WriteLine($"Today is {now}");
